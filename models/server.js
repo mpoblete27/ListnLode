047324f2494533d72bf9d5f71fc7819da1f83293
ArpeggiMode <39497523+ArpeggiMode@users.noreply.github.com>
2019-02-05 13:08:11 -0800

    do not merge until we find out why LF is being replaced by CLRF

---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('test connection', 'root', "", {
+  host: 'localhost',
+  dialect: 'mysql'|'sqlite'|'postgres'|'mssql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  });
+  
+const Passport = require("passport");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
