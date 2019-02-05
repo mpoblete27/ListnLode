089e8460531b6695fe96f817861f4dc6602222ab
ArpeggiMode <39497523+ArpeggiMode@users.noreply.github.com>
2019-02-05 14:59:36 -0800

    new file structure and runnable backend

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const path = require("path");
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize('test connection', 'root', "", {
-  host: 'localhost',
-  dialect: 'mysql'|'sqlite'|'postgres'|'mssql',
-  operatorsAliases: false,
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  });
   
 const Passport = require("passport");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+var db = require("./models");
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -28,6 +17,8 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
