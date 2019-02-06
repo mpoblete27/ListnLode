ab0eb90934614858d628b9bf2fb513359ce3f402
ArpeggiMode <39497523+ArpeggiMode@users.noreply.github.com>
2019-02-06 14:09:55 -0800

    db exports and songArray pseudocode

--- models/uploads.js
+++ models/uploads.js
@@ -1,20 +1,57 @@
-// hi
+module.exports = function(sequelize, DataTypes) {
 var Uploads = sequelize.define('uploads', {
-    upload_id: {
-        type: sequelize.INTEGER,
+    id: {
+        type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
     track_name: {
-        type: sequelize.STRING
+        type: DataTypes.STRING
     },
     user_id: {
-        type: sequelize.INTEGER
+        type: DataTypes.INTEGER
     },
     number_of_pins: {
-        type: sequelize.STRING
+        type: DataTypes.STRING
     },
     upload_url: {
-        type: sequelize.STRING
+        type: DataTypes.STRING
     }
 });
+
+Uploads.associate = function(models) {
+    Uploads.belongsTo(models.Users, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+    Uploads.hasMany(models.Pins, {
+        onDelete: "cascade"
+    })
+  };
+
+// var songArray = [];
+// 
+// var songToUpload = ??;
+// 
+// var currentSong = songArray[?];
+// 
+// 
+// var uploadSong = (function(uploads), {
+//  
+// (songArray.push(songToUpload); 
+// 
+// });
+// 
+// 
+// var play = (function(uploads), {
+// currentSong = songArray[?];
+// if (playingSong === true{
+// play(); };
+// else { console.log("that is not playing."); }
+// 
+// });
+// 
+// 
+return Uploads;
+};
