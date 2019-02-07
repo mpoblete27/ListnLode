db1be3781881cf62e25c052f3d40e319eb77507f
Allain Bernal <llnbrnl017@gmail.com>
2019-02-06 22:23:36 -0800

    3880

--- client/src/Components/Pages/Welcome/Welcome.js
+++ client/src/Components/Pages/Welcome/Welcome.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as  Route } from "react-router-dom";
 import Jumbotron from "../../Jumbotron";
+import CarouselPage from "../../CarouselPage/CarouselPage";
 import Authentication from "./Authentication/Authentication";
 import Login from "./Login/Login";
 
@@ -8,10 +9,17 @@ const Welcome = () => {
   // implement user logic in backend doc when signup is complete
   return (
     <div>
-      <Jumbotron />
-      <Authentication />
-      <Route path="/Login" component={Login} />
-      <Route path="*" component={Authentication} />
+      <CarouselPage />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8"><Jumbotron /></div>
+          <div class="col-md-4">
+            <Authentication />
+            <Route path="/Login" component={Login} />
+            <Route path="*" component={Authentication} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
